a0faf8665d7463b0c4ce67b46c0e3f7d3675847b
mmafights <zahrajroblox@gmail.com>
2025-12-05 14:56:05 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyByDiqnvIwnwm2YgnXoqRKC4N79xGOQS18",
+  authDomain: "stream-89ee2.firebaseapp.com",
+  projectId: "stream-89ee2",
+  storageBucket: "stream-89ee2.firebasestorage.app",
+  messagingSenderId: "705861977770",
+  appId: "1:705861977770:web:303d3a4fc57a449431f2d3",
+  measurementId: "G-RZWFYNC6B3"
+};
